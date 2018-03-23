15675e9fc8db289620bdd553959a0d049704d1a6
Ivan Driuk <veytarn@gmail.com>
2018-03-23 07:17:09 +0200

    add cors for cloud frontend project

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(cors({
+  origin: 'https://idriuk.github.io/cloud/'
+}));
+
 
 app.get('/', (req, res) => res.send('Check auto deploy to heroku'))
 

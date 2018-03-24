fbc2011c41429f7e3cf31b2c02959f9b92489ac8
Ivan Driuk <veytarn@gmail.com>
2018-03-24 10:41:10 +0200

    use cors only on heroku

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-  origin: 'https://idriuk.github.io'
-}));
+if (process.env.NODE_ENV == 'production') {
+  app.use(cors({
+    origin: 'https://idriuk.github.io'
+  }));
+}
 
+app.get('/', (req, res) => res.send('test response'))
 
-app.get('/', (req, res) => res.send('Check auto deploy to heroku'))
-
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Api listening on port ${PORT}`))

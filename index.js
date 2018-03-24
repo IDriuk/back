91414cd83480403e50131cad1cc1ee1dbf7e5ce1
Ivan Driuk <veytarn@gmail.com>
2018-03-24 19:12:39 +0200

    change default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 if (process.env.NODE_ENV == 'production') {
   app.use(cors({
